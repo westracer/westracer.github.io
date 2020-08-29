c6f0702e520d750d28342d726aab747a1b277128
Igor Kharakhordin <westracer1@gmail.com>
2020-08-29 23:56:19 +0700

    Updates

---
+++ precache-manifest.e1b34e788e91184f8699e3311aecf5a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2bd2d54f2357b918d29122cf20574c",
+    "url": "/westracer.github.io/index.html"
+  },
+  {
+    "revision": "f3e6b2337e6ab98b0473",
+    "url": "/westracer.github.io/static/css/main.f2474586.chunk.css"
+  },
+  {
+    "revision": "e1a53843a4c1b6202303",
+    "url": "/westracer.github.io/static/js/2.04f95132.chunk.js"
+  },
+  {
+    "revision": "6afc98da8ac0066813bba005b875d2e5",
+    "url": "/westracer.github.io/static/js/2.04f95132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e6b2337e6ab98b0473",
+    "url": "/westracer.github.io/static/js/main.4f639edf.chunk.js"
+  },
+  {
+    "revision": "88a436644af69479a78d",
+    "url": "/westracer.github.io/static/js/runtime-main.3184e2f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/westracer.github.io/precache-manifest.a50606356295c32ac3b4620e555d62d1.js"
+  "/westracer.github.io/precache-manifest.e1b34e788e91184f8699e3311aecf5a6.js"
 );
 
 self.addEventListener('message', (event) => {
